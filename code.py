c520a947162deed66211e278a6dff85388f79449
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2020-12-28 14:13:06 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,65 +1,45 @@
-from datetime import date
 import time
 import os
-
-# Get date
-date = date.today()
-date_str = str(date)
+import modes
+import loader
+import numcheck
+import file_handler
 
 # Vars to change
-itemsList = ["N/A", "Ford", "Volvo", "BMW"]
-priceList = [0.00, 100, 20, 300]
 fileName = "Output.txt"
 fileDir = "/home/pi/Documents/Code/py-piprint"
 file = fileDir + "/" + fileName
 print_command = "lp -o fit-to-page " + file
-removeFile = True
+removeFile = False
 
 # More Vars
 printingText = "Printing"
 titleText = "Shopping"
 
+# Vars you probably don't need to change
 def clear(): os.system('clear')
+
 clear()
 
 # Items list and length
-length = len(itemsList) - 1
-length_str = str(length)
-print(itemsList[1:4])
-selected_item = input("Enter a product id between 1 and " + length_str + ":")
-selected_item_int = int(selected_item)
-price = str(priceList[selected_item_int])
+item_int = numcheck.check()
+i = item_int
+if i == 1:
+    item = modes.choose()
+    file_handler.new(fileName, item, titleText)
+if i == 2:
+    item = modes.choose2()
+    file_handler.new2(fileName, item, titleText)
+if i == 3:
+    item = modes.choose3()
+    file_handler.new3(fileName, item, titleText)
 
 # Create file
-with open(fileName,'w',encoding = 'utf-8') as f:
-   f.write(titleText + "\n")
-   f.write("------------------\n")
-   f.write("Item")
-   f.write("        ")
-   f.write("Price\n")
-   f.write("------------------\n")
-   f.write(itemsList[selected_item_int] + "           " + price + "\n")
-   f.write("------------------\n")
-   f.write("Printed on " + date_str)
-   f.close()
+
 
 # Print file
 # res = os.system(print_command)
-clear()
-print(printingText + " - [....]")
-time.sleep(0.4)
-clear()
-print(printingText + " - [#...]")
-time.sleep(0.4)
-clear()
-print(printingText + " - [##..]")
-time.sleep(0.4)
-clear()
-print(printingText + " - [###.]")
-time.sleep(0.4)
-clear()
-print(printingText + " - [####]")
-clear()
+loader.load(printingText)
 if removeFile:
   os.remove(file)
 print("Complete")
